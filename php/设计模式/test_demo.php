b54e7145d8babce2c37e1f1bd5a77905d6732848
unknown <zt20134@zongteng.me>
2021-07-05 16:57:16 +0800

    工厂模式：简单工厂，抽象工厂，工厂方法

--- php/设计模式/test_demo.php
+++ php/设计模式/test_demo.php
@@ -1,5 +1,121 @@
 <?php
 
+
+////抽象产品
+//class Button{}
+////抽象产品
+//class Border{}
+//
+////具体产品
+//class MacButton extends Button{}
+////具体产品
+//class WinButton extends Button{}
+////具体产品
+//class MacBorder extends Border{}
+////具体产品
+//class WinBorder extends Border{}
+//
+////抽象工厂
+//interface AbstractFactory {
+//    public function CreateButton();
+//    public function CreateBorder();
+//}
+////具体工厂
+//class MacFactory implements AbstractFactory{
+//    public function CreateButton(){ return new MacButton(); }
+//    public function CreateBorder(){ return new MacBorder(); }
+//}
+////具体工厂
+//class WinFactory implements AbstractFactory{
+//    public function CreateButton(){ return new WinButton(); }
+//    public function CreateBorder(){ return new WinBorder(); }
+//}
+
+
+//抽象产品
+class Button
+{
+    public function get (){
+        echo '具体产品';
+    }
+}
+//具体产品
+class WinButton extends Button
+{
+
+}
+
+//具体产品
+class MacButton extends Button
+{
+}
+
+//抽象工厂
+interface ButtonFactory
+{
+    public function createButton($type);
+}
+
+//具体工厂
+class MyButtonFactory implements ButtonFactory
+{
+    // 实现工厂方法
+    public function createButton($type)
+    {
+        switch ($type) {
+            case 'Mac':
+                return new MacButton();
+            case 'Win':
+                return new WinButton();
+        }
+    }
+}
+
+$button_obj = new MyButtonFactory();
+var_dump($button_obj->createButton('Mac')->get());
+var_dump($button_obj->createButton('Win')->get());
+
+/*
+//具体产品角色
+class web
+{
+    public function create()
+    {
+        echo "web";
+    }
+}
+//具体产品角色
+
+class app
+{
+    public function create()
+    {
+        echo "app";
+    }
+
+}
+
+//工厂角色
+class factory
+{
+    //抽象产品角色
+    public function ConcreteProduct($key)
+    {
+        if ($key == 'air') {
+            return new web();
+        }
+        if ($key == 'app') {
+            return new app();
+        }
+    }
+}
+$factory = new factory();
+$app = $factory->ConcreteProduct('app');
+$app->create();
+
+
+
+
 //抽象被观察者
 abstract class Subject
 {
@@ -65,3 +181,5 @@ $server->addObserver($app);
 
 //被观察者发布消息
 $server->send();
+
+*/
