3e6ac193a7afae43410b323e43e16a71fba4fb59
unknown <zt20134@zongteng.me>
2021-07-19 15:02:15 +0800

    php 设计模式 代理模式

--- php/设计模式/test_demo.php
+++ php/设计模式/test_demo.php
@@ -1,5 +1,46 @@
 <?php
 
+abstract class Subject { // 抽象主题角色
+    abstract public function action();
+}
+class RealSubject extends Subject { // 真实主题角色
+    public function __construct() {}
+    public function action() {
+        echo '真实 action';
+    }
+}
+class ProxySubject extends Subject { // 代理主题角色
+    private $_real_subject = NULL;
+    public function __construct() {}
+    public function action() {
+        $this->_beforeAction();
+        if (is_null($this->_real_subject)) {
+            $this->_real_subject = new RealSubject();
+        }
+        $this->_real_subject->action();
+        $this->_afterAction();
+    }
+    private function _beforeAction() {
+        echo '在action前,我想干点啥....';
+    }
+    private function _afterAction() {
+        echo '在action后,我还想干点啥....';
+    }
+}
+// client
+$subject = new ProxySubject();
+$subject->action();
+
+
+
+
+
+
+
+
+
+
+die;
 /**
  * 创建渲染接口。
  * 这里的装饰方法 renderData() 返回的是字符串格式数据。
